84f1511873aaebacc402be599da27bbab21e495b
sedrickamote <sedrickopulencil@gmail.com>
2026-08-04 13:21:55 +0800

    initial setup: React + TypeScript + Tailwind + Login page

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
